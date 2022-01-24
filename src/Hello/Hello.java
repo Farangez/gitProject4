0fab41a1a65aef876aaf5aebc260e580d31130e3
Farangez Hikmatova <Fhikmatova@gmail.com>
2022-01-24 10:42:18 -0500

    doing final work

--- src/Hello/Hello.java
+++ src/Hello/Hello.java
@@ -4,5 +4,9 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello world");
+
+        System.out.println("B25 Learning GIT and GitHub");
+
     }
 }
+
